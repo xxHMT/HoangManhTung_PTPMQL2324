7bf862ab1a3d5e29f336e029205ee06ba71c03f7
xxHMT <algoroku@gmail.com>
2023-08-15 11:46:23 +0700

    first commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// int a = 9;
+// int b = 3;
+// Console.WriteLine("int {0} + {1} = {2} ", a, b, a + b);
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string HoTen = "Hoang Manh Tung";
+        int Tuoi = 22;
+        Console.WriteLine("Sinh vien {0} - {1} tuoi", HoTen, Tuoi);
+    }
+}
