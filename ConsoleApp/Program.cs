3ad97777bf8d405ca208bd2f6593fd3251e32a0e
xxHMT <algoroku@gmail.com>
2023-08-19 11:09:04 +0700

    If_Else

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,20 +7,23 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int a, b;
+        int a = 10;
 
-        System.Console.Write("a = ");// Nhap vao a;
+        if (a < 0)
+        {
+            System.Console.WriteLine("{0} la so nguyen am", a);
+        }
+        else
+        {
+            if (a % 2 == 0)
+            {
+                System.Console.WriteLine("{0} la so nguyen duong chan", a);
+            }
+            else
+            {
+                System.Console.WriteLine("{0} la so nguyen duong le", a);
+            }
 
-        a = Convert.ToInt32(Console.ReadLine()); // Chuyen doi kieu du lieu vua nhap sang int va gan cho a
-
-        System.Console.Write("b = ");
-
-        b = Convert.ToInt32(Console.ReadLine());
-
-        System.Console.WriteLine("{0} + {1} = {2}", a, b, a + b);
-        System.Console.WriteLine("{0} - {1} = {2}", a, b, a - b);
-        System.Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
-        System.Console.WriteLine("{0} / {1} = {2}", a, b, a / b);
-        System.Console.WriteLine("{0} % {1} = {2}", a, b, a % b);
+        }
     }
 }
