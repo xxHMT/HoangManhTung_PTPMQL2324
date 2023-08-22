4cbe95307d5ad5628c0ce19d6ddd705f20322244
xxHMT <algoroku@gmail.com>
2023-08-22 09:44:59 +0700

    Object

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NewApp.Models;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Person ps1 = new Person();
+        Person ps2 = new Person();
+
+        ps1.FullName = "Hoang Van D";
+        ps1.Address = "Phu Tho";
+        ps1.Age = 35;
+
+        ps2.FullName = "Nguyen Thao M";
+        ps2.Address = "Ha Noi";
+        ps2.Age = 18;
+
+    
+
+        ps1.Display();
+        ps2.Display();
+
+    }
+}
