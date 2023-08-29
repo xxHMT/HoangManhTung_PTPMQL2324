fa3cf0bfd67d749b9473d6fafe8347e1ec818c62
xxHMT <algoroku@gmail.com>
2023-08-29 23:24:08 +0700

    Fruit

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -5,34 +5,12 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        //Khai bao mang 
-        int n = 0;
+        Fruit fruit = new Fruit();
 
-        do
-        {
-            System.Console.WriteLine("Nhap vao n:");
-            n = Convert.ToInt32(Console.ReadLine());
-        } while (n < 1);
+        fruit.EnterData();
+        fruit.Display();
 
-        SinhVien [] svArr = new SinhVien[n];
-        for(int i = 0; i < svArr.Length; i++)
-        {
-            System.Console.WriteLine("Nhap vao phan tu thu " + (i + 1));
 
-            SinhVien sv = new SinhVien();
-            
-            // Nhap thong tin
-
-            sv.EnterData();
-
-            svArr[i] = sv;
-        }
-
-        // Hien thi
-        foreach (SinhVien sv in svArr)
-        {
-            sv.Display();
         }
 }
 
-}
